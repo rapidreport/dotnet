9d0cfa2d71742c77f1703cf27e582ce653e06aa5
Shuetsu Ito <shuetsu@systembase.co.jp>
2015-10-23 15:00:50 +0900

    4.20

--- renderer.pdf/My Project/AssemblyInfo.vb
+++ renderer.pdf/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' 既定値にすることができます:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("4.18.0.0")> 
-<Assembly: AssemblyFileVersion("4.18.0.0")> 
+<Assembly: AssemblyVersion("4.20.0.0")> 
+<Assembly: AssemblyFileVersion("4.20.0.0")> 
