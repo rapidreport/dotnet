a2285b19f53b6ae3e330bffebc68112ec068e030
Shuetsu Ito <shuetsu@systembase.co.jp>
2016-04-13 18:01:20 +0900

    4.23

--- report/My Project/AssemblyInfo.vb
+++ report/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' 既定値にすることができます:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("4.22.0.0")> 
-<Assembly: AssemblyFileVersion("4.22.0.0")> 
+<Assembly: AssemblyVersion("4.23.0.0")> 
+<Assembly: AssemblyFileVersion("4.23.0.0")> 
