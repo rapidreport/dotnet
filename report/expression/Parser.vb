733c1ed86434ed0ebe25e53fbd110d33deec30de
Shuetsu Ito <shuetsu@systembase.co.jp>
2016-07-28 17:17:03 +0900

    クオートで囲んだ形式のメソッドパラメータに対応

--- report/expression/Parser.vb
+++ report/expression/Parser.vb
@@ -17,7 +17,7 @@ Namespace expression
         Private Shared REGEX_NUMBER As New Regex("^-?[0-9.]+$")
         Private Shared REGEX_DATE As New Regex("^#([0-9]{4})([0-9]{2})([0-9]{2})$")
         Private Shared REGEX_DATETIME As New Regex("^#([0-9]{4})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})$")
-        Private Shared REGEX_METHOD As New Regex("^([^.#@]*)?(\.([^.#@]*))?(@([^.#@]*))?(#([^.#@]*))?$")
+        Private Shared REGEX_METHOD As New Regex("^([^.#@]*)?(\.((\'.*\')|([^.#@]*)))?(@([^.#@]*))?(#([^.#@]*))?$")
 
         Private index As Integer
         Private setting As ReportSetting
@@ -45,8 +45,6 @@ Namespace expression
         Private Function _Parse_aux(ByVal source As String) As IExpression
             If source(Me.index) = "("c Then
                 Return Me._Parse_operator(source)
-            ElseIf source(Me.index) = "'"c Then
-                Return Me._Parse_text(source)
             Else
                 Dim token As String = Me.nextToken(source)
                 If token.Equals(LITERAL_NULL) Then
@@ -55,6 +53,8 @@ Namespace expression
                     Return New ImmediateExpression(True)
                 ElseIf token.Equals(LITERAL_FALSE) Then
                     Return New ImmediateExpression(False)
+                ElseIf token.StartsWith("'") Then
+                    Return New ImmediateExpression(evalString(token))
                 End If
                 With Nothing
                     Dim m As Match = REGEX_NUMBER.Match(token)
@@ -88,24 +88,31 @@ Namespace expression
                     If m.Success Then
                         Dim methodKey As String = m.Groups(1).ToString.Trim
                         Dim method As IMethod = Nothing
-                        Dim param As String = m.Groups(3).ToString.Trim
-                        Dim scope As String = m.Groups(5).ToString.Trim
-                        Dim unit As String = m.Groups(7).ToString.Trim
+                        Dim param As String = Nothing
+                        Dim param1 As String = m.Groups(4).ToString.Trim
+                        Dim param2 As String = m.Groups(5).ToString.Trim
+                        Dim scope As String = m.Groups(7).ToString.Trim
+                        Dim unit As String = m.Groups(9).ToString.Trim
                         If String.IsNullOrEmpty(methodKey) Then
                             methodKey = Nothing
                         End If
-                        method = Me.setting.GetMethod(methodKey)
-                        If method Is Nothing Then
-                            Throw New EvalException("メソッド '" & methodKey & "' は見つかりません : " & source)
+                        If Not String.IsNullOrEmpty(param1) Then
+                            param = evalString(param1)
+                        ElseIf Not String.IsNullOrEmpty(param2) Then
+                            param = param2
                         End If
                         If String.IsNullOrEmpty(param) Then
                             param = Nothing
                         End If
-                        If m.Groups(4).Length = 0 Then
+                        If m.Groups(6).Length = 0 Then
                             scope = Nothing
                         End If
-                        If m.Groups(6).Length = 0 Then
+                        If m.Groups(8).Length = 0 Then
                             unit = Nothing
+                        End If
+                        method = Me.setting.GetMethod(methodKey)
+                        If method Is Nothing Then
+                            Throw New EvalException("メソッド '" & methodKey & "' は見つかりません : " & source)
                         End If
                         RaiseEvent CreateMethod(method, methodKey, param, scope, unit)
                         Return New MethodExpression(method, param, scope, unit)
@@ -122,9 +129,6 @@ Namespace expression
             If operatorKey.Length = 0 Then
                 Throw New EvalException("オペレータがありません : " & source)
             End If
-            If operatorKey.StartsWith("(") Then
-                Throw New EvalException("'(' は予期せぬ文字です : " & source)
-            End If
             Dim [operator] As IOperator = Me.setting.GetOperator(operatorKey)
             If [operator] Is Nothing Then
                 Throw New EvalException("オペレータ '" & operatorKey & "' は見つかりません : " & source)
@@ -135,55 +139,13 @@ Namespace expression
                 If Me.index = source.Length Then
                     Throw New EvalException("')' がありません : " & source)
                 End If
+
                 If source(Me.index) = ")"c Then
                     Me.index += 1
                     Return New OperatorExpression([operator], params)
                 End If
                 params.Add(Me._Parse_aux(source))
             Loop
-        End Function
-
-        Private Function _Parse_text(ByVal source As String) As IExpression
-            Me.index += 1
-            Dim i As Integer = Me.index
-            Dim sb As New StringBuilder
-            Dim escaped As Boolean = False
-            Do
-                If i >= source.Length Then
-                    Throw New EvalException("文字列が閉じられていません : " & source)
-                End If
-                Dim c As Char = source(i)
-                If c = "'"c And Not escaped Then
-                    If i > Me.index Then
-                        sb.Append(source.Substring(Me.index, i - Me.index))
-                        Me.index = i
-                    End If
-                    Exit Do
-                End If
-                If escaped Then
-                    Select Case c
-                        Case "'"
-                            sb.Append("'")
-                        Case "\"
-                            sb.Append("\")
-                        Case "n"
-                            sb.Append(vbCrLf)
-                        Case Else
-                            Throw New EvalException("不正なエスケープ文字です: \" & c & " (有効なもの \' \\ \n) : " & source)
-                    End Select
-                    escaped = False
-                    Me.index = i + 1
-                ElseIf c = "\"c Then
-                    If i > Me.index Then
-                        sb.Append(source.Substring(Me.index, i - Me.index))
-                        Me.index = i
-                    End If
-                    escaped = True
-                End If
-                i += 1
-            Loop
-            Me.index += 1
-            Return New ImmediateExpression(sb.ToString)
         End Function
 
         Private Sub skipSpace(ByVal source As String)
@@ -194,8 +156,32 @@ Namespace expression
 
         Private Function nextToken(ByVal source As String) As String
             Dim i As Integer = Me.index
-            Do While i < source.Length AndAlso _
-              source(i) <> " "c AndAlso source(i) <> ")"c
+            Dim escaped As Boolean = False
+            Dim quoted As Boolean = False
+            Do While i < source.Length
+                Dim c As Char = source(i)
+                If Not escaped And Not quoted Then
+                    If c = " "c Or c = ")"c Then
+                        Exit Do
+                    End If
+                End If
+                If Not escaped Then
+                    If c = "\" Then
+                        escaped = True
+                    Else
+                        If Not quoted Then
+                            If c = "'" Then
+                                quoted = True
+                            End If
+                        Else
+                            If c = "'" Then
+                                quoted = False
+                            End If
+                        End If
+                    End If
+                Else
+                    escaped = False
+                End If
                 i += 1
             Loop
             Dim ret As String = ""
@@ -204,6 +190,52 @@ Namespace expression
                 Me.index = i
             End If
             Return ret
+        End Function
+
+        Private Function evalString(ByVal token As String) As String
+            Dim i As Integer = 1
+            Dim j As Integer = 1
+            Dim sb As New StringBuilder
+            Dim escaped As Boolean = False
+            Do
+                If j >= token.Length Then
+                    Throw New EvalException("文字列が閉じられていません : " & token)
+                End If
+                Dim c As Char = token(j)
+                If Not escaped Then
+                    If c = "'"c Then
+                        If j > i Then
+                            sb.Append(token.Substring(i, j - i))
+                            i = j
+                        End If
+                        Exit Do
+                    ElseIf c = "\"c Then
+                        If j > i Then
+                            sb.Append(token.Substring(i, j - i))
+                            i = j
+                        End If
+                        escaped = True
+                    End If
+                Else
+                    Select Case c
+                        Case "'"
+                            sb.Append("'")
+                        Case "\"
+                            sb.Append("\")
+                        Case "n"
+                            sb.Append(vbCrLf)
+                        Case Else
+                            Throw New EvalException("不正なエスケープ文字です: \" & c & " (有効なもの \' \\ \n) : " & token)
+                    End Select
+                    escaped = False
+                    i = j + 1
+                End If
+                j += 1
+            Loop
+            If i + 1 < token.Length Then
+                Throw New EvalException("'" & token.Substring(i + 1, 1) & "'は予期せぬ文字です : " & token)
+            End If
+            Return sb.ToString
         End Function
 
     End Class
