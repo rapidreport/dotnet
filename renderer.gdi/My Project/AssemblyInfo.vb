42c7ed7d4e0fffbb3cb3769fd6c0a8b388f00ad1
Shuetsu Ito <shuetsu@systembase.co.jp>
2017-12-01 17:02:29 +0900

    4.33

--- renderer.gdi/My Project/AssemblyInfo.vb
+++ renderer.gdi/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' 既定値にすることができます:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("4.32.2.0")>
-<Assembly: AssemblyFileVersion("4.32.2.0")>
+<Assembly: AssemblyVersion("4.33.0.0")>
+<Assembly: AssemblyFileVersion("4.33.0.0")>
