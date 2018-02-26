c59815821c21afa5b693e12768a319868c9de1d8
Shuetsu Ito <shuetsu@systembase.co.jp>
2018-02-26 15:21:01 +0900

    4.35.1

--- renderer.xls/My Project/AssemblyInfo.vb
+++ renderer.xls/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' 既定値にすることができます:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("4.35.0.0")>
-<Assembly: AssemblyFileVersion("4.35.0.0")>
+<Assembly: AssemblyVersion("4.35.1.0")>
+<Assembly: AssemblyFileVersion("4.35.1.0")>
