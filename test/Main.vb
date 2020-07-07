41f0f9c90cead23cf1f5a761e8806395935ca6f5
ykoji <ykoji@systembase.co.jp>
2020-07-07 10:31:43 +0900

    Int16を数値として判断しない問題のテストを追加

--- test/Main.vb
+++ test/Main.vb
@@ -59,6 +59,7 @@
         Tests.Add(New Test_5_5_Wsub)
         Tests.Add(New Test_5_6_CharSpacing)
         Tests.Add(New Test_5_7_YbCode)
+        Tests.Add(New Test_5_7_IsNumeric)
 
         Application.Run(New FmTest)
     End Sub
---
+++ test/Test_5_7_IsNumeric.vb
@@ -0,0 +1,54 @@
+﻿Imports System.IO
+Imports jp.co.systembase.json
+Imports jp.co.systembase.report
+Imports jp.co.systembase.report.data
+Imports jp.co.systembase.report.renderer.gdi
+Imports jp.co.systembase.report.renderer.xls
+Imports jp.co.systembase.report.renderer.xlsx
+Imports NPOI.HSSF.UserModel
+Imports NPOI.XSSF.UserModel
+
+Public Class Test_5_7_IsNumeric
+
+    Public Overrides Function ToString() As String
+        Return "5.7 IsNumeric"
+    End Function
+
+    Public Sub Run()
+        Dim name As String = "test_5_7_isnumeric"
+
+        Dim report As New Report(Json.Read("rrpt\" & name & ".rrpt"))
+        report.Fill(New ReportDataSource(_GetDataTable()))
+
+        Dim pages As ReportPages = report.GetPages()
+        Dim preview As New FmPrintPreview(New Printer(pages))
+        preview.ShowDialog()
+    End Sub
+
+    Private Function _GetDataTable() As DataTable
+        Dim ret As New DataTable()
+
+        ret.Columns.Add("vInteger", GetType(Integer))
+        ret.Columns.Add("vUInteger", GetType(UInteger))
+
+        ret.Columns.Add("vLong", GetType(Long))
+        ret.Columns.Add("vULong", GetType(ULong))
+
+        ret.Columns.Add("vSingle", GetType(Single))
+        ret.Columns.Add("vDouble", GetType(Double))
+
+        ret.Columns.Add("vDecimal", GetType(Decimal))
+
+        ret.Columns.Add("vSByte", GetType(SByte))
+        ret.Columns.Add("vByte", GetType(Byte))
+
+        ret.Columns.Add("vShort", GetType(Short))
+        ret.Columns.Add("vUShort", GetType(UShort))
+
+        ret.Rows.Add(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+        ret.Rows.Add(9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9)
+
+        Return ret
+    End Function
+
+End Class
