33a121064f7c11ab9c4153b31fd9f00259ac2ff4
shuetsu <shuetsu@systembase.co.jp>
2021-07-01 10:02:49 +0900

    テストにフォント設定を行うコードを追加

--- test/Test_0_1.vb
+++ test/Test_0_1.vb
@@ -11,6 +11,7 @@ Imports jp.co.systembase.report.renderer.gdi
 Imports jp.co.systembase.report.renderer.pdf
 Imports jp.co.systembase.report.renderer.xls
 Imports jp.co.systembase.report.renderer.xlsx
+Imports iTextSharp.text.pdf
 
 Public Class Test_0_1
 
@@ -21,6 +22,8 @@ Public Class Test_0_1
     Public Sub Run()
         Dim name As String = "test_0_1"
 
+        Dim fontPath As String = System.Environment.GetFolderPath(Environment.SpecialFolder.Fonts)
+
         Dim report As New Report(Json.Read("rrpt\" & name & ".rrpt"))
         report.GlobalScope.Add("time", Now)
         report.GlobalScope.Add("lang", "vb")
@@ -30,12 +33,14 @@ Public Class Test_0_1
 
         Using fs As New FileStream("out\" & name & ".pdf", IO.FileMode.Create)
             Dim renderer As New PdfRenderer(fs)
+            renderer.Setting.FontMap.Add("meiryo", BaseFont.CreateFont(fontPath & "\\meiryo.ttc,0", BaseFont.IDENTITY_H, BaseFont.EMBEDDED))
             pages.Render(renderer)
         End Using
 
         Using fs As New FileStream("out\" & name & ".xls", IO.FileMode.Create)
             Dim workbook As New HSSFWorkbook
             Dim renderer As New XlsRenderer(workbook)
+            renderer.Setting.FontMap.Add("meiryo", "メイリオ")
             renderer.NewSheet(name)
             pages.Render(renderer)
             workbook.Write(fs)
@@ -44,12 +49,15 @@ Public Class Test_0_1
         Using fs As New FileStream("out\" & name & ".xlsx", IO.FileMode.Create)
             Dim workbook As New XSSFWorkbook
             Dim renderer As New XlsxRenderer(workbook)
+            renderer.Setting.FontMap.Add("meiryo", "メイリオ")
             renderer.NewSheet(name)
             pages.Render(renderer)
             workbook.Write(fs)
         End Using
 
-        Dim preview As New FmPrintPreview(New Printer(pages))
+        Dim printer As New Printer(pages)
+        printer.Setting.FontMap.Add("meiryo", "メイリオ")
+        Dim preview As New FmPrintPreview(printer)
         preview.ShowDialog()
     End Sub
 
